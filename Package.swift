09874dd177ba01abc2231c330b160522b24e9124
Cho sungbok <gging200@gradehc.com>
2023-07-31 15:25:13 +0900

    change dependency version

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
             targets: ["SignalR-Swift"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire.git", "5.0.0"..<"5.4.4"),
-        .package(url: "https://github.com/daltoniam/Starscream.git", "3.0.0"..<"4.0.0")        
+        .package(url: "https://github.com/Alamofire/Alamofire.git", "5.0.0"..<"6.0.0"),
+        .package(url: "https://github.com/daltoniam/Starscream.git", "3.0.0"..<"3.1.2")        
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
